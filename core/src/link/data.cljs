92d884cd0f8ec3b58e3a74810f2e4d314157be3f
Will Acton <will@willacton.com>
2021-10-17 14:30:52 -0700

    init

---
+++ core/src/link/data.cljs
@@ -0,0 +1,140 @@
+(ns link.data
+  (:require
+   [autonormal.core :as a]
+   [clojure.set :as c.set]))
+
+
+(defprotocol IDataCache
+  (-add-query-watch [o query f]
+    "Runs `f` anytime the results of `query` change.
+Returns the result of running `query`")
+  (-update-query-entities [o query entities]
+    "Updates the set of entities that `queries` contains, to handle notifying
+watchers of `query` when `entities` change.")
+  (-notify-entity-watches! [o query entities]
+    "Notifies any watches of `query`, and any watches of queries that contain
+any of `entities`, of changes.")
+  (-remove-query-watch [o query f]
+    "Removes `f` from watches on `query`. Cleans up `query` completely if it's
+the last watcher."))
+
+
+(defn update!
+  ([tcoll key f x]
+   (assoc! tcoll key (f (get tcoll key) x)))
+  ([tcoll key f x y & xs]
+   (assoc! tcoll key (apply f (get tcoll key) x y xs))))
+
+
+(def conj-set
+  (fnil conj #{}))
+
+
+;; TODO delete data / cache eviction
+(deftype DataCache [state query-watches entity->queries]
+  Object
+  (equiv [this other]
+    (-equiv this other))
+
+  IEquiv
+  (-equiv [o other]
+    (identical? o other))
+
+  IDeref
+  (-deref [_] state)
+
+  IDataCache
+  (-add-query-watch [this query f]
+    (let [{:keys [data entities]} (a/pull-report state query)
+          entity->queries' (transient entity->queries)]
+      ;; add entity => query
+      (doseq [entity entities]
+        (update! entity->queries' entity conj-set query))
+      (set! (.-entity->queries this) (persistent! entity->queries'))
+      ;; TODO should we notify query listeners of changes to `entities`?
+      ;; add query => entities, f
+      (set! (.-query-watches this)
+            (-> query-watches
+                (assoc-in [query :entities] entities)
+                (update-in [query :fs] conj-set f)))
+      data))
+
+  (-remove-query-watch [this query f]
+    (let [{:keys [entities fs]} (get query-watches query)
+          fs' (disj fs f)]
+      (if (seq fs')
+        (set! (.-query-watches this) (assoc-in query-watches [query :fs] fs'))
+        ;; if we have no more fs watching query, remove query from entities
+        ;; and then clear out query from query-watches completely to avoid
+        ;; memory leak
+        (let [entity->queries' (transient entity->queries)]
+          (doseq [entity entities]
+            (update! entity->queries' entity disj query))
+          (set! (.-entity->queries this) (persistent! entity->queries'))
+          (set! (.-query-watches this) (dissoc query-watches query))))))
+
+  (-update-query-entities [this query new-entities]
+    ;; look up query => entities, fs
+    ;; set difference new-entities, old-entities
+    ;; disj query from rm-entities
+    ;; conj query to new-entities
+    (let [{:keys [entities]} (get query-watches query)
+          rm-entities (c.set/difference entities new-entities)
+          add-entities (c.set/difference new-entities entities)]
+      ;; see if we have work to do
+      (when (or (seq rm-entities) (seq add-entities))
+        (let [entity->queries' (transient entity->queries)]
+          (doseq [entity rm-entities]
+            (update! entity->queries' entity disj query))
+          (doseq [entity add-entities]
+            (update! entity->queries' entity conj-set query))
+          (set! (.-entity->queries this) (persistent! entity->queries'))
+          (set!
+           (.-query-watches this)
+           (assoc-in query-watches [query :entities] new-entities)))) ))
+
+  (-notify-entity-watches! [_ query entities]
+    (let [all-fs (into
+                  (get-in query-watches [query :fs])
+                  (comp
+                   (mapcat #(get entity->queries %))
+                   (mapcat #(get-in query-watches [% :fs])))
+                  entities)]
+      ;; TODO we rely on `f` to pull the data here. we could speed this up
+      ;; by pulling here instead and calling each `f` for each query, so we
+      ;; only pull each query once (in the case where multiple `f`s are watching
+      ;; the same query)
+      (doseq [f all-fs]
+        (f state)))))
+
+
+(defn data-cache
+  ([] (data-cache (a/db [])))
+  ([db] (->DataCache db {} {})))
+
+
+(defn add-data!
+  "Adds new data to the normalized cache, and notifies anyone listening to the
+  entities the data pertains of changes."
+  [dc query data]
+  (let [old-state (.-state dc)
+        {:keys [db entities]} (a/add-report old-state data)]
+    (set! (.-state dc) db)
+    (when-not (nil? (get-in (.-query-watches dc) [query :fs]))
+      (-update-query-entities dc query entities)
+      (-notify-entity-watches! dc query entities))
+    db))
+
+
+(defn subscribe!
+  "Returns a tuple `[data unsubscribe]`"
+  [dc query f]
+  (let [prev-data (atom nil)
+        f' (fn [state]
+             (let [{:keys [data entities]} (a/pull-report state query)]
+               (when (not= @prev-data data)
+                 (reset! prev-data data)
+                 (-update-query-entities dc query entities)
+                 (f data))))
+        unsub #(-remove-query-watch dc query f')]
+    [(-add-query-watch dc query f') unsub]))
---
+++ core/test/link/data_test.cljs
@@ -0,0 +1,104 @@
+(ns link.data-test
+  (:require
+   [clojure.test :as t]
+   [link.data :as l.d]))
+
+
+(defn spy
+  []
+  (let [calls (atom {:count 0})]
+    [calls #(doto calls
+              (swap! update :count inc)
+              (swap! assoc :data %))]))
+
+
+(t/deftest query-subscribe
+  (t/testing "query without entities updates when data is added"
+    (let [dc (l.d/data-cache)
+          query [{:foo [:bar]}]
+          test-data {:foo {:bar "baz"}}
+          [calls f] (spy)
+          [data unsub] (l.d/subscribe! dc query f)]
+      (t/is (= {} data))
+      (t/is (= 0 (:count @calls)))
+      (l.d/add-data! dc query test-data)
+      (t/is (= 1 (:count @calls)))
+      (t/is (= test-data (:data @calls)))
+      (t/testing "doesn't call f after unsub"
+        (unsub)
+        (l.d/add-data! dc query (assoc-in test-data [:foo :bar] "asdf"))
+        (t/is (= 1 (:count @calls)))
+        (t/is (= test-data (:data @calls))))))
+
+  (t/testing "2 queries sharing entities both update when data is added for one"
+    (let [dc (l.d/data-cache)
+          query1 [{:foo [:bar/id :baz]}]
+          query2 [{:asdf [:bar/id :baz :jkl]}]
+          test-data1 {:foo {:bar/id 0 :baz 42}}
+          test-data2 {:asdf {:bar/id 0 :baz 42 :jkl "qwerty"}}
+          test-data3 {:foo {:bar/id 0 :baz 100}}
+          test-data4 {:asdf {:bar/id 0 :baz 100 :jkl "uiop"}}
+          [query1-calls query1-f] (spy)
+          [query2-calls query2-f] (spy)
+          [data1 unsub1] (l.d/subscribe! dc query1 query1-f)
+          [data2 unsub2] (l.d/subscribe! dc query2 query2-f)]
+      (t/is (= {} data1))
+      (t/is (= {} data2))
+      (t/is (= 0 (:count @query1-calls)))
+      (t/is (= 0 (:count @query2-calls)))
+
+      ;; add query1-data
+      (l.d/add-data! dc query1 test-data1)
+      (t/is (= 1 (:count @query1-calls)))
+      (t/is (= test-data1 (:data @query1-calls)))
+      (t/is (= 0 (:count @query2-calls)))
+      ;; query2-f hasn't been called yet
+      (t/is (= nil (:data @query2-calls)))
+
+      ;; add query2-data
+      (l.d/add-data! dc query2 test-data2)
+      ;; query1-f isn't called since pull result is equal
+      (t/is (= 1 (:count @query1-calls)))
+      ;; still equal to test-data1, because it's the same attr values as test-data2
+      (t/is (= test-data1 (:data @query1-calls)))
+      ;; did see changes in query2
+      (t/is (= 1 (:count @query2-calls)))
+      (t/is (= test-data2 (:data @query2-calls)))
+
+      ;; add new query1-data
+      (l.d/add-data! dc query2 test-data3)
+      (t/is (= 2 (:count @query1-calls)))
+      (t/is (= test-data3 (:data @query1-calls)))
+      (t/is (= 2 (:count @query2-calls)))
+      (t/is (= (assoc-in test-data2 [:asdf :baz] 100)
+               (:data @query2-calls)))
+
+      ;; add new query2-data
+      (l.d/add-data! dc query2 test-data4)
+      ;; query1 result is same again so isn't called and data is unchanged
+      (t/is (= 2 (:count @query1-calls)))
+      (t/is (= test-data3 (:data @query1-calls)))
+      ;; did see changes in query2
+      (t/is (= 3 (:count @query2-calls)))
+      (t/is (= test-data4
+               (:data @query2-calls)))
+
+      (t/testing "doesn't call f after unsub"
+        (unsub1)
+        (l.d/add-data! dc query2 test-data2)
+        (t/is (= 2 (:count @query1-calls)) "query1-f isn't called after unsub")
+        (t/is (= test-data3 (:data @query1-calls)))
+
+        (t/is (= 4 (:count @query2-calls)))
+        (t/is (= test-data2 (:data @query2-calls)))
+
+        (unsub2)
+
+        (t/is (empty? (.-query-watches dc)) "cleans up query-watches")
+        (t/is (empty? (get (.-entity->queries dc) [:bar/id 0]))
+              "cleans up entity->queries relations")))))
+
+
+(comment
+  (t/run-tests)
+  )
