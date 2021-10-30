73a40c3f86d0c0c9758b325802f7a2296239f514
Will Acton <will@willacton.com>
2021-10-30 00:32:44 -0700

    fix subscribe-status!, parameterize-query

--- core/src/exo/core.cljs
+++ core/src/exo/core.cljs
@@ -1,7 +1,10 @@
 (ns exo.core
   (:require
+   [clojure.string :as string]
    [exo.data :as data]
-   [exo.network.core :as net]))
+   [exo.network.core :as net]
+   [edn-query-language.core :as eql]
+   [clojure.zip :as zip]))
 
 
 (defn create-config
@@ -34,10 +37,12 @@
                 ;; start a new one
                 *stored-req
                 (-> query
+                    (eql/query->ast)
                     (network opts)
-                    (doto (net/-then
-                           (fn [data]
-                             (data/add-data! data-cache query data))))
+                    (net/-then
+                     (fn [data]
+                       (data/add-data! data-cache query data)
+                       data))
                     (net/request->ref)))]
      (swap! request-store assoc-in [query opts] *req)
      *req)))
@@ -65,24 +70,77 @@
   ([config query f]
    (subscribe-status! config query {} f))
   ([config query opts f]
-   (let [k (gensym "exo-status")
+   (let [outer-key (gensym "exo-req-store")
+         inner-key (gensym "exo-req-status")
+         request-store (:request-store config)
+         inner-req (get-in @request-store [query opts])
          ;; need to store current req to unsub
-         current-req (atom nil)
-         request-store (:request-store config)]
+         current-req (atom inner-req)]
+
+     ;; subscribe to the request immediately if available
+     (when inner-req
+       (add-watch
+        inner-req inner-key
+        (fn [_ _ _ n]
+          (f (:status n)))))
+
+     ;; watch for new requests added for this query
      (add-watch
-      request-store k
+      request-store outer-key
       (fn [_ _ o n]
         (let [old-req (get-in o [query opts])
               new-req (get-in n [query opts])]
           (when (not= old-req new-req)
             (f (:status @new-req))
+            #_(when old-req (remove-watch old-req inner-key))
             (add-watch
              new-req
-             k
+             inner-key
              (fn [_ _ _ n]
                (f (:status n))))
+            ;; update ref to current req for unsub
             (reset! current-req new-req)))))
      (fn unsub-status []
+       (remove-watch request-store outer-key)
        (when-let [*req @current-req]
-         (remove-watch *req k)
-         (remove-watch request-store k))))))
+         (remove-watch *req inner-key))))))
+
+
+(defn- make-node
+  [node children]
+  (if (map-entry? node)
+    (into [] children)
+    (into (empty node) (if (list? node)
+                         (reverse children) ; grumble
+                         children))))
+
+
+(defn- query-zipper
+  [query]
+  (zip/zipper coll? seq make-node query))
+
+
+(defn- query-var?
+  [x]
+  (and
+   (symbol? x)
+   (string/starts-with? (str x) "?")))
+
+
+(defn- resolve-query-var
+  [qv bindings]
+  (get bindings (-> (str qv) (subs 1) (keyword))))
+
+
+(defn parameterize-query
+  [query bindings]
+  (loop [loc (query-zipper query)]
+    (if (zip/end? loc)
+      (zip/root loc)
+      (let [node (zip/node loc)]
+        (recur
+         (cond-> loc
+           (query-var? node)
+           (zip/edit resolve-query-var bindings)
+
+           true (zip/next)))))))
