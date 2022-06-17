f93dd67856a9e2f82aeb5ef00e8c58bd68790265
Will Acton <will@willacton.com>
2022-06-17 12:50:16 -0700

    format hooks.cljs

--- hooks/src/exo/hooks.cljs
+++ hooks/src/exo/hooks.cljs
@@ -15,63 +15,62 @@
   ([query {:keys [enabled?]
            :or {enabled? true}}]
    (let [config (r/useContext exo-config-context)
-        query-hash (hash query)
-        ;; we need to provide a synchronous, unscoped way of getting the results
-        ;; of a query, rather than relying on subscribe! to pass the value to
-        ;; some callback.
-        ;; we use a ref as that store in lieu of a way to just get a memoized
-        ;; result of a query from the exo data-cache
-        ;; TODO cache whole result in data-cache so that we don't rely on pull
-        results-store (r/useRef
+         query-hash (hash query)
+         ;; we need to provide a synchronous, unscoped way of getting the results
+         ;; of a query, rather than relying on subscribe! to pass the value to
+         ;; some callback.
+         ;; we use a ref as that store in lieu of a way to just get a memoized
+         ;; result of a query from the exo data-cache
+         ;; TODO cache whole result in data-cache so that we don't rely on pull
+         results-store (r/useRef
                        ;; memoize this on mount because it's expensive
-                       (r/useMemo
-                        #(exo.data/pull
-                          (:data-cache config)
-                          query)
-                        #js []))
-        subscribe-data
-        (r/useCallback
-         (fn [cb]
-           (let [[result unsub] (exo.data/subscribe!
-                                 (:data-cache config) query
-                                 (fn [result]
-                                   (when (not= (.-current results-store) result)
-                                     (set! (.-current results-store) result))
-                                   (cb)))]
-             ;; we may have subscribed before initiating getting data,
-             ;; so if we have results put them in the store now
-             (when (not= (.-current results-store) result)
-               (set! (.-current results-store) result))
-             unsub))
-         #js [query-hash config])
+                        (r/useMemo
+                         #(exo.data/pull
+                           (:data-cache config)
+                           query)
+                         #js []))
+         subscribe-data
+         (r/useCallback
+          (fn [cb]
+            (let [[result unsub] (exo.data/subscribe!
+                                  (:data-cache config) query
+                                  (fn [result]
+                                    (when (not= (.-current results-store) result)
+                                      (set! (.-current results-store) result))
+                                    (cb)))]
+              ;; we may have subscribed before initiating getting data,
+              ;; so if we have results put them in the store now
+              (when (not= (.-current results-store) result)
+                (set! (.-current results-store) result))
+              unsub))
+          #js [query-hash config])
 
-        subscribe-status
-        (r/useCallback
-         (fn [cb]
-           (exo/subscribe-status!
-            config query
-            cb))
-         #js [query-hash config])
+         subscribe-status (r/useCallback
+                           (fn [cb]
+                             (exo/subscribe-status!
+                              config query
+                              cb))
+                           #js [query-hash config])
 
-        data (useSyncExternalStore
-              subscribe-data
-              #(.-current results-store))
+         data (useSyncExternalStore
+               subscribe-data
+               #(.-current results-store))
 
-        status (useSyncExternalStore
-                subscribe-status
-                #(exo/current-status config query))
+         status (useSyncExternalStore
+                 subscribe-status
+                 #(exo/current-status config query))
 
-        load (r/useCallback
-              #(exo/preload! config query)
-              #js [query-hash config])]
-    (r/useEffect
-     #(when enabled?
-        (load))
-     #js [load enabled?])
-    {:data data
-     :status status
-     :loading? (= net/loading status)
-     :refetch load})))
+         load (r/useCallback
+               #(exo/preload! config query)
+               #js [query-hash config])]
+     (r/useEffect
+      #(when enabled?
+         (load))
+      #js [load enabled?])
+     {:data data
+      :status status
+      :loading? (= net/loading status)
+      :refetch load})))
 
 
 (defn use-config
